2c854f63c7ebab84bf76a7b4221d2935afd731e6
kamonpob.p <kamonpob.p@linecorp.com>
2020-05-17 17:35:00 +0700

    feat: create repo via cli

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple cURL
+
+
+```curl
+curl -u 'username' https://api.github.com/user/repos -d "{\"name\":\"github-create-repo-by-cli\",\"private\":false}"
+
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const shell = require('shelljs')
+const argv = require('yargs')
+    .usage('Usage: git-create-repo')
+    .options({
+      'u': {
+        alias: 'user',
+        describe: '--user [username]',
+        demandOption: true
+      },
+      'repo': {
+        alias: 'repo',
+        describe: '--private [repository name]',
+        demandOption: true
+      },
+      'private': {
+        alias: 'private',
+        describe: '--private [optional create private repo]'
+      }
+    })
+    .argv
+
+const curl = `curl -u '${argv.u}' https://api.github.com/user/repos -d "{\\\"name\\\":\\\"${argv.repo}\\\",\\\"private\\\":${argv.private === undefined ? false : true}}"`
+shell.exec(curl)
